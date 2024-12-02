8f34d4bf7b039d26db1685e33e8a2386fb96b7e1
Aaron Aster <aarongoodm@gmail.com>
2024-12-02 11:39:28 -0500

    day 2 part 2

--- aoc/src/day_2/main.rs
+++ aoc/src/day_2/main.rs
@@ -2,19 +2,43 @@ use utils::{parse_iter, AocBufReader};
 
 fn main() {
     part_1(AocBufReader::from_string("aoc/src/day_2/data/part_1.txt"));
+    part_2(AocBufReader::from_string("aoc/src/day_2/data/part_1.txt"));
 }
 
 fn part_1(input: AocBufReader) {
     println!("part 1: {}", part_1_inner(input));
 }
 
+fn part_2(input: AocBufReader) {
+    println!("part 2: {}", part_2_inner(input));
+}
+
 fn part_1_inner(input: impl Iterator<Item = String>) -> usize {
-    input.filter(|x| is_safe(x.to_owned())).count()
+    input
+        .filter(|x| is_safe(parse_iter::<usize, &str>(x.split_whitespace())))
+        .count()
+}
+
+fn part_2_inner(input: impl Iterator<Item = String>) -> usize {
+    input
+        .filter(|x| {
+            let vals: Vec<usize> = parse_iter::<usize, &str>(x.split_whitespace()).collect();
+            let mut to_skip = vec![usize::MAX];
+            to_skip.extend(0..vals.len());
+            to_skip.into_iter().any(|skip_idx| {
+                is_safe(
+                    vals.iter()
+                        .enumerate()
+                        .filter(|(idx, _)| *idx != skip_idx)
+                        .map(|(_, val)| *val),
+                )
+            })
+        })
+        .count()
 }
 
 #[allow(clippy::comparison_chain)]
-fn is_safe(s: String) -> bool {
-    let mut vals = parse_iter::<usize, &str>(s.split_whitespace());
+fn is_safe(mut vals: impl Iterator<Item = usize>) -> bool {
     let first = vals.next();
     let second = vals.next();
     let is_increasing: bool = match (first, second) {
@@ -66,9 +90,22 @@ mod tests {
             "1 3 2 4 5",
             "8 6 4 4 1",
             "1 3 6 7 9",
-            "2 5 6 7 7",
         ];
 
         assert_eq!(part_1_inner(input.into_iter().map(|x| x.to_string())), 2);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = [
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9",
+        ];
+
+        assert_eq!(part_2_inner(input.into_iter().map(|x| x.to_string())), 4);
     }
 }
